4242a8f4eea843016222acae93b8b0656e1135af
Atechchatterjee <atechchatterjeee@gmail.com>
2022-07-13 21:53:19 +0530

    style: add custom scroll bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import '../styles/globals.css';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import "../styles/globals.css";
+import ScrollBarWrapper from "../components/ScrollBarWrapper";
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ScrollBarWrapper>
+        <Component {...pageProps} />
+      </ScrollBarWrapper>
     </ChakraProvider>
   );
-}
+};
 
 export default MyApp;
---
+++ util/ScrollBarStyle.ts
@@ -0,0 +1,31 @@
+interface ScrollBarStyleProps { 
+  color?: string;
+  borderRadius?: "sm" | "md" | "lg";
+  hidden?: boolean;
+  scrollbarWidth?: string;
+}
+
+export const scrollBarStyle = ({ hidden, color, borderRadius, scrollbarWidth }: ScrollBarStyleProps = {}) => ({
+  "&::-webkit-scrollbar": {
+    width: "0.9rem",
+    height: "0.5em",
+    borderRadius:  borderRadius ? borderRadius: "full",
+    bg: "gray.200",
+    display: hidden ? "none": "block"
+  },
+  "&::-webkit-scrollbar-track": {
+    display: "none",
+    backgroundColor: "rgba(255,255,255,0)",
+    borderRadius:  borderRadius ? borderRadius: "full",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    height: "3px",
+    border: "3px solid #E2E8F0",
+    borderRadius:  borderRadius ? borderRadius: "full",
+    backgroundColor: color || "primary.200",
+    transition: "background-color 0.8s ease-in-out",
+    display: hidden ? "none": "block"
+  },
+  // hide scrollbar for firefox
+  scrollbarWidth: scrollbarWidth || ""
+});
