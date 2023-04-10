db6991b9180a1aaaf7e218d26a63c734099da40d
Atechchatterjee <atechchatterjeee@gmail.com>
2023-04-10 19:24:05 +0530

    style: add css for hover effects

--- theme/index.ts
+++ theme/index.ts
@@ -12,11 +12,19 @@ export default extendTheme({
           bg: "#3C59C6",
           color: "white",
           fontSize: "sm",
+          _hover: {
+            bg: "#3C59C6e0",
+          },
         },
         "outline-blue": {
           border: "1px solid #3C59C6",
           color: "#3C59C6",
           fontSize: "sm",
+          transition: "all 0.4s ease-in-out",
+          _hover: {
+            bg: "#3C59C6",
+            color: "white",
+          },
         },
       },
     },
