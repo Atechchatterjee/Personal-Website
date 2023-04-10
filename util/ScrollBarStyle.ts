27874c105c82dad0e19a150cfdb0f47ed7bbb549
Atechchatterjee <atechchatterjeee@gmail.com>
2023-04-10 19:22:58 +0530

    style: change bg color for scrollbar

--- util/ScrollBarStyle.ts
+++ util/ScrollBarStyle.ts
@@ -10,7 +10,7 @@ export const scrollBarStyle = ({ hidden, color, borderRadius, scrollbarWidth }: 
     width: "0.9rem",
     height: "0.5em",
     borderRadius:  borderRadius ? borderRadius: "full",
-    bg: "gray.200",
+    bg: "gray.100",
     display: hidden ? "none": "block"
   },
   "&::-webkit-scrollbar-track": {
@@ -22,7 +22,7 @@ export const scrollBarStyle = ({ hidden, color, borderRadius, scrollbarWidth }: 
     height: "3px",
     border: "3px solid #E2E8F0",
     borderRadius:  borderRadius ? borderRadius: "full",
-    backgroundColor: color || "primary.200",
+    backgroundColor: color || "gray.400",
     transition: "background-color 0.8s ease-in-out",
     display: hidden ? "none": "block"
   },
